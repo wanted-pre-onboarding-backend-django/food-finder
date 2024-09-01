ac3499e5f12d96eea730e35353a13e3363879f3b
Jihyun-Choi <034179@naver.com>
2024-09-01 23:17:44 +0900

    feat: postsql 설정 추가

--- src/config/settings.py
+++ src/config/settings.py
@@ -65,6 +65,7 @@ INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + PACKAGE_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -99,8 +100,15 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("POSTGRES_DB", default="foodfinderdb"),
+        "USER": env("POSTGRES_USER", default="postgres"),
+        "PASSWORD": env("POSTGRES_PASSWORD", default="password"),
+        "HOST": env("POSTGRESQL_HOST", default="postgres"),
+        "PORT": env("POSTGRES_PORT"),
+        "TEST": {
+            "NAME": "mytestdb",
+        },
     }
 }
 
@@ -141,6 +149,15 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "src", "staticfiles")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "src", "media")
+
+# WHITENOISE Setting
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_MANIFEST_STRICT = False
+WHITENOISE_ALLOW_ALL_ORIGINS = True
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
