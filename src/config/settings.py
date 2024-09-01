b71b6eb79f2281c10e60d589627333cbac05f5df
Hess <hess90wang@gmail.com>
2024-09-01 22:13:13 +0900

    fix: 캐싱 테스트 확인을 위한 캐시 직렬화 코드 삭제

--- src/config/settings.py
+++ src/config/settings.py
@@ -162,7 +162,6 @@ CACHES = {
         "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-            "SERIALIZER": "django_redis.serializers.json.JSONSerializer",
         },
     }
 }
