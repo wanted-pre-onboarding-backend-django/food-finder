fdc32da998b098874437ae365e50ae82a9fb25cd
dollyishere <rundollyrun8@gmail.com>
2024-09-03 05:51:29 +0900

    feat: celery를 통한 data-pipline scheduling 구현

--- src/config/celery.py
+++ src/config/celery.py
@@ -4,19 +4,23 @@ from celery.schedules import crontab
 from datetime import timedelta
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-app = Celery('config')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+app = Celery("config")
 app.config_from_object(
     "django.conf:settings",
     namespace="CELERY",
 )
 app.autodiscover_tasks()
-app.conf.timezone = 'Asia/Seoul'
+app.conf.timezone = "Asia/Seoul"
 
 app.conf.beat_schedule = {
-    # TODO: run_full_pipeline task 설정
-    # "test-schedule": {
-    #     "task": "config.tasks.run_full_pipeline",
-    #     'schedule': timedelta(minutes=2),
-    # }
+    # data_pipline 작업 매주 월요일 0시 0분에 실행
+    "test-schedule": {
+        "task": "config.tasks.run_full_pipeline",
+        "schedule": crontab(
+            minute=0,
+            hour=0,
+            day_of_week="monday",
+        ),
+    }
 }
